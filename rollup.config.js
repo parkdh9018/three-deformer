bd6a6cbcc3e42e7598977160993b8e734f1e4573
parkdn9018 <parkdh9018@naver.com>
2024-12-12 15:41:09 +0900

    build : set up typescript

--- rollup.config.js
+++ rollup.config.js
@@ -4,7 +4,7 @@ import commonjs from '@rollup/plugin-commonjs'
 import terser from '@rollup/plugin-babel'
 import json from '@rollup/plugin-json'
 export default {
-    input: 'src/index.js',
+    input: 'src/index.ts',
     output: [
         {
             file: 'dist/bundle.cjs.js',
