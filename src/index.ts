f2ff4fa1a19b36a3b98e89a257fc3d81ea86eeac
parkdn9018 <parkdh9018@naver.com>
2024-12-17 18:14:00 +0900

    build : set up eslint, prettier

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
-import { version } from "../package.json";
+import { version } from '../package.json';
 
 export default function () {
-  console.log("version " + version);
+  console.log('version ' + version);
 }
