041097902e7d21211a4df74dd9b849cd83045093
parkdn9018 <parkdh9018@naver.com>
2025-03-10 21:26:58 +0900

    build: set three package + fix rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -1,38 +1,42 @@
-import babel from '@rollup/plugin-babel'
-import resolve from '@rollup/plugin-node-resolve'
-import commonjs from '@rollup/plugin-commonjs'
-import terser from '@rollup/plugin-babel'
-import json from '@rollup/plugin-json'
-import serve from 'rollup-plugin-serve'
-import livereload from 'rollup-plugin-livereload'
+import del from 'rollup-plugin-delete';
+
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import babel from '@rollup/plugin-babel';
+import typescript from '@rollup/plugin-typescript';
+import terser from '@rollup/plugin-terser';
+import serve from 'rollup-plugin-serve';
+import livereload from 'rollup-plugin-livereload';
+import json from '@rollup/plugin-json';
+
+const isProduction = !process.env.ROLLUP_WATCH;
 
 export default {
     input: 'src/index.ts',
     output: [
-        {
-            file: 'dist/bundle.cjs.js',
-            format: 'cjs',
-        },
-        {
-            file: 'dist/bundle.esm.js',
-            format: 'esm',
-        },
+        { file: 'dist/bundle.cjs.js', format: 'cjs' },
+        { file: 'dist/bundle.esm.js', format: 'esm' }
     ],
+    external: ['three'],
     plugins: [
+        del( { targets: 'dist/*' } ),
         json(),
-        resolve(),
+        resolve({
+            extensions: ['.js', '.ts']
+        }),
         commonjs(),
+        typescript(),
         babel({
             babelHelpers: 'bundled',
-            presets: ['@babel/preset-env'],
+            presets: ['@babel/preset-env']
         }),
-        terser(),
-        serve( {
+        isProduction && terser(),
+        !isProduction && serve({
             open: true,
-            contentBase: [ 'public', '.' ],
+            contentBase: ['public', '.'],
             openPage: '/',
             port: 3000,
-        } ),
-        livereload('dist')
-    ],
-}
+        }),
+        !isProduction && livereload('dist'),
+    ]
+};
