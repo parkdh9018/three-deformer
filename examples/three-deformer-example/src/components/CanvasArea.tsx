386b1f11752feba43e33f1ff79d0cc6c3f240d53
parkdn9018 <parkdh9018@naver.com>
2025-03-26 18:30:22 +0900

    feat: twist example page

--- examples/three-deformer-example/src/components/CanvasArea.tsx
+++ examples/three-deformer-example/src/components/CanvasArea.tsx
@@ -1,30 +1,29 @@
-import { OrbitControls } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import { Deformer } from 'three-deformer'
+import { Canvas } from '@react-three/fiber';
 import * as THREE from 'three';
+import { DeformerController } from './DeformerController';
+import { Deformer } from 'three-deformer';
 
 export const CanvasArea = () => {
-  
-  const geometry = new THREE.BoxGeometry( 2, 2, 2, 32, 32, 32 );
-  const material = new THREE.MeshPhongMaterial( {
+  const geometry = new THREE.BoxGeometry(2, 2, 2, 32, 32, 32);
+  const material = new THREE.MeshPhongMaterial({
     color: 'orange',
-    flatShading: true
-  } );
-  const mesh = new THREE.Mesh( geometry, material );
+    flatShading: true,
+  });
 
+  const mesh = new THREE.Mesh(geometry, material);
   const deformer = new Deformer(mesh);
 
-  deformer.addTwist({direction : 'x'});
+  deformer.addTwist();
   deformer.apply();
-  
-  deformer.changeWeight('twist', 0.5)
 
   return (
-    <Canvas style={{ width : '100vw', height : '100vh'}} camera={{ position: [2, 2, 5], fov: 60 }}>
+    <Canvas
+      style={{ width: '100vw', height: '100vh' }}
+      camera={{ position: [2, 2, 5], fov: 60 }}
+    >
       <ambientLight />
       <directionalLight position={[3, 3, 3]} />
-      <primitive object={mesh} />
-      <OrbitControls/>
+      <DeformerController mesh={mesh} deformer={deformer} />
     </Canvas>
-  )
-}
+  );
+};
