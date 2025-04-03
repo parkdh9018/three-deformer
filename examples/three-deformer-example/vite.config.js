f8231f3a0a83270f5b122700b1c25341be9c4ced
parkdn9018 <parkdh9018@naver.com>
2025-04-03 23:19:35 +0900

    chore: fix gitignore + build option

--- examples/three-deformer-example/vite.config.js
+++ examples/three-deformer-example/vite.config.js
@@ -6,7 +6,8 @@ import tailwindcss from '@tailwindcss/vite';
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   build: {
-    outDir: 'dist',
+    outDir: 'build',
     emptyOutDir: true,
-  }
+  },
+  base: '/three-deformer/',
 });
