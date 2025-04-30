b09c60a41df65c52f2e20b220b79e3acdca2830e
parkdn9018 <parkdh9018@naver.com>
2025-04-30 17:44:49 +0900

    feat: highlight API section on hash anchor navigation

--- examples/three-deformer-example/src/components/2. API/methods/template/MethodTemplate.tsx
+++ examples/three-deformer-example/src/components/2. API/methods/template/MethodTemplate.tsx
@@ -1,3 +1,5 @@
+import { useLocation } from 'react-router-dom';
+
 type Props = {
   functionName: string;
   title: string;
@@ -5,13 +7,20 @@ type Props = {
 };
 
 export const MethodTemplate = ({ functionName, title, children }: Props) => {
+  const location = useLocation();
+  const hash = location.hash;
+  const isActive = hash === `#${functionName}`;
+
   return (
     <div className="mb-10 scroll-mt-20">
       <div className="mb-1">
         <a
           id={functionName}
           href={`#${functionName}`}
-          className="link-hover link-info text-lg font-semibold"
+          className={
+            'link-hover link-info text-lg font-semibold' +
+            (isActive ? ' text-success' : '')
+          }
         >
           {title}
         </a>
