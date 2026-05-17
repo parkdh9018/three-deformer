3914c9f87a55c3f710611f795246db3271560793
parkdh9018 <parkdh9018@naver.com>
2026-05-18 01:03:04 +0900

    fix: prevent livereload in example build

--- examples/three-deformer-example/src/main.tsx
+++ examples/three-deformer-example/src/main.tsx
@@ -4,10 +4,15 @@ import { RecoilRoot } from 'recoil';
 import App from './App';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
+const basename =
+  import.meta.env.BASE_URL === '/'
+    ? undefined
+    : import.meta.env.BASE_URL.replace(/\/$/, '');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <RecoilRoot>
-      <BrowserRouter basename="/three-deformer">
+      <BrowserRouter basename={basename}>
         <Routes>
           <Route path="*" element={<App />} />
         </Routes>
